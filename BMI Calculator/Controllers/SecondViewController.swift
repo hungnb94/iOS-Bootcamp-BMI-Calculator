1af584e8f2e4b615f9c2f035081831c042e6020d
Hung Nguyen <hungnb94@gmail.com>
2023-11-13 22:09:05 +0700

    Create second view controller

---
+++ BMI Calculator/Controllers/SecondViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SecondViewController.swift
+//  BMI Calculator
+//
+//  Created by Hung Ba on 13/11/2023.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
