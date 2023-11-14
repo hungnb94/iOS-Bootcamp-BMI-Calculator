9ff5f02ef1c808f37d5a815adbaf7675b6e7d873
Hung Nguyen <hungnb94@gmail.com>
2023-11-14 22:49:09 +0700

    Open result screen

--- BMI Calculator/Controllers/CalculateViewController.swift
+++ BMI Calculator/Controllers/CalculateViewController.swift
@@ -8,33 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CalculateViewController: UIViewController {
     @IBOutlet weak var heightLabel: UILabel!
     @IBOutlet weak var weightLabel: UILabel!
     @IBOutlet weak var heightSlider: UISlider!
     @IBOutlet weak var weightSlider: UISlider!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
     @IBAction func heightSliderChanged(_ sender: UISlider) {
         heightLabel.text = String(format: "%.2fm", sender.value)
     }
-
+    
     @IBAction func weightSliderChanged(_ sender: UISlider) {
         weightLabel.text = String(format: "%.0fkg", sender.value)
     }
-
+    
     @IBAction func calculatePressed(_ sender: UIButton) {
-        let height = heightSlider.value
-        let weight = weightSlider.value
-        let bmi = weight / (height * height)
-        
-        let secondVC = SecondViewController()
-        secondVC.bmi = String(format: "%.f", bmi)
-        self.present(secondVC, animated: true, completion: nil)
+        self.performSegue(withIdentifier: "goToResult", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToResult" {
+            let viewController = segue.destination as! ResultViewController
+            
+            let height = heightSlider.value
+            let weight = weightSlider.value
+            let bmi = weight / (height * height)
+            
+            viewController.bmiValue = String(format: "%.1f", bmi)
+        }
     }
 }
 
---
+++ BMI Calculator/Controllers/ResultViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ResultViewController.swift
+//  BMI Calculator
+//
+//  Created by Hung Ba on 14/11/2023.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    @IBOutlet weak var bmiLabel: UILabel!
+    @IBOutlet weak var adviceLabel: UILabel!
+    
+    var bmiValue = "0.0"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("bmi = \(bmiValue)")
+    }
+
+}
